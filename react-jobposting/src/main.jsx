607672c2bbb17da015e98395c768580c2178eee9
Zarin Subah <133192570+Zarin21@users.noreply.github.com>
2025-09-15 21:19:30 -0600

    HTML theme code

--- react-jobposting/src/main.jsx
+++ react-jobposting/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // Wrapper component for highlighting potential problems
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
--- react-jobposting/vite.config.js
+++ react-jobposting/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // React that we selected
-  // by default it's 5173.
+  plugins: [
+    [react()], // React that we selected
+    // by default it's 5173.
+    tailwindcss(),
+  ],
   server: {
-    port: 3000, // server object
+    port: 3000, // server
   },
 })
